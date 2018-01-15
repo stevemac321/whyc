3d710097d19d1e7f6cdef41a1d0bf793bf4b17a1
Stephen MacKenzie <stevemac321@live.com>
2018-01-15 13:13:19 -0800

    kandr quicksort

--- sortc_inl/main.c
+++ sortc_inl/main.c
@@ -1,4 +1,3 @@
-#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,52 +8,43 @@ static inline int compare(const void *x, const void *y)
 {
 	return *(int *)x - *(int *)y;
 }
-static inline void partition(double *v, int lower, int upper, int *pivot)
+static inline void swap(int v[], int i, int j)
 {
-	double x = v[lower];
-	int up = lower + 1; /* index will go up */
-	int down = upper;   /* index will go down */
-	while (up < down) {
-		while ((up < down) && (v[up] <= x))
-			up++;
-		while ((up < down) && (v[down] > x))
-			down--;
-		if (up == down)
-			break;
-		double tmp = v[up];
-		v[up] = v[down];
-		v[down] = tmp;
-	}
-	if (v[up] > x)
-		up--;
-	v[lower] = v[up];
-	v[up] = x;
-	*pivot = up;
+        int tmp;
+        tmp = v[i];
+        v[i] = v[j];
+        v[j] = tmp;
 }
-static inline void quicksort(double *v, int start, int end)
+static inline void quicksort(int v[], int left, int right)
 {
-	if (start < end) {
-		int pivot;
-		partition(v, start, end, &pivot);
-		quicksort(v, start, pivot - 1);
-		quicksort(v, pivot + 1, end);
-	}
+        int i, last;
+        if(left >= right)
+                return;
+
+        swap(v, left, (left + right) / 2);
+        last = left;
+
+        for(i = left + 1; i <= right; i++)
+                if(v[i] < v[left])
+                        swap(v, ++last, i);
+
+        swap(v, left, last);
+        quicksort(v, left, last-1);
+        quicksort(v, last+1, right);
 }
 int main()
 {
-	omp_set_num_threads(8);
 	size_t pivot = NUM_ELEMS;
 	clock_t start, stop;
-	double v[NUM_ELEMS];
+	int  v[NUM_ELEMS];
         int n = NUM_ELEMS - 1;
 
 	srand(time(NULL));
 	for (int i = 0; i < NUM_ELEMS; i++)
 		v[i] = rand() % NUM_ELEMS - 100;
 
-#pragma omp parallel
 	start = clock();
-        quicksort(&v[0], 0, NUM_ELEMS - 1);
+        quicksort(v, 0, NUM_ELEMS - 1);
 	stop = clock();
 
 	printf("%.4lf\n", (stop - start) / ((double)CLOCKS_PER_SEC));
